c808ef3de719e70c6ce765d4933e522b944ef0c6
Craig Nicholson <craig.nicholson@gmail.com>
2018-08-03 08:29:28 -0400

    Update cenos 7 install script

--- 03_Orchestration/centos7_dockerInstall.sh
+++ 03_Orchestration/centos7_dockerInstall.sh
@@ -1,8 +1,12 @@
+# ssh into your server
+# run this script, and enter your sudo pwd when prompted
+# sudo bash <(curl -s https://raw.githubusercontent.com/craignicholson/docker-quickstart/master/03_Orchestration/centos7_dockerInstall.sh)
 sudo yum install -y yum-utils device-mapper-persistent-data  lvm2
-sudo yum-complete-transaction -y
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install docker-ce -y
 sudo groupadd docker
 sudo usermod -aG  docker $USER
-sudo systemctl enable docker && sudo systemctl start docker && sudo systemctl status docker
+sudo systemctl enable docker && sudo systemctl start docker 
+sudo yum-complete-transaction -y
+#sudo systemctl status docker
 #sudo systemctl disable firewalld && sudo systemctl stop firewalld
